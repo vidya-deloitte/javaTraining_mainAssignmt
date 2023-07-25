941f29d4f47b36d50f34f326b6c8d79f58fdd53e
vidye <vidye@deloitte.com>
2023-07-25 20:55:38 +0530

    first

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String args[])
+    {
+        while(true){
+            int inputChoice;
+            System.out.println("Menu\n");
+            System.out.println("1.Admin\n2.Employee\n3.Exit\n");
+            System.out.println("Please enter choice:");
+            Scanner input=new Scanner(System.in);
+            inputChoice=input.nextInt();
+            switch(inputChoice){
+                case 1:break;
+                case 2:break;
+                case 3:break;
+            }
+
+
+
+
+        }
+
+    }
+}
